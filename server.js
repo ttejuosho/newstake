7d22bfbeef983408e4bc16a87f70dc29cef21cae
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2017-11-13 23:46:32 -0600

    plus

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect("mongodb://localhost/newstake", {
 // This route will retrieve all of the data
 // from the scrapedData collection as a json (this will be populated
 // by the data you scrape using the next route)
-app.get("/", function(req,res){
+app.get("/scrape", function(req,res){
     
       request("https://www.nytimes.com/section/technology", function(error, response, html){
     
